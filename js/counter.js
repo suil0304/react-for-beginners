eb8cdd9416a15591047fb45f73e1006de139216b
suil0304 <eagle030410@gmail.com>
2026-07-05 11:41:30 +0900

    #2.6

--- js/counter.js
+++
@@ -1,9 +0,0 @@
-const clicker = document.querySelector("#clicker");
-const counter = document.querySelector("#counter");
-
-let clickCount = 0;
-
-clicker.addEventListener("click", () => {
-    clickCount++;
-    counter.innerText = clickCount;
-});
